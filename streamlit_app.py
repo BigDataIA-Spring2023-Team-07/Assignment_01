91cd5aec0aaef76a4a82fe8ddb06a7b12d42bf2b
Subhash Chandran <shankarakumar.s@northeastern.edu>
2023-02-10 14:46:00 -0500

    Nexrad locations added

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,31 @@
 import streamlit as st
 from PIL import Image
+import pandas as pd
 
 from pages import Nexrad
+from pages import Nexrad_plot
+import sqlite3
+import os
+
+def create_plot_table():
+    """creates data base leveraging the dataframe created
+    """
+    # with open(ddl_file_path, 'r') as sql_file:
+        # sql_script = sql_file.read()
+    database_file_name = "assignment_01.db"
+    database_file_path = os.path.join('data/',database_file_name)
+    db = sqlite3.connect(database_file_path)
+    cursor = db.cursor()
+    cursor.execute('''CREATE TABLE if not exists NEXRAD_PLOT (Id,State,City,ICAO_Location_Identifier,Coordinates,Lat,Lon)''')
+    df = pd.read_csv("data/Nexrad.csv")
+    df["Lon"] = -1 * df["Lon"]
+    df.to_sql('nexrad_plot', db, if_exists='append', index = False)
+    db.commit()
+    db.close()
+
 
 if __name__ == "__main__":
+    create_plot_table()
     st.title("Assignment 1 Team 07")
     image = Image.open('image.png')
     st.image(image, caption='four humans working on satelite data from the moon')
